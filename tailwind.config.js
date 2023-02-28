4aea44454a1796cd34ee64e72821b522e6f6047f
hjw4595 <hongjung73@gmail.com>
2023-03-01 01:37:20 +0900

    add : 안쓰는 파일 삭제 , tailwind 기본 설정

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app/**/*.{html,js,tsx,ts}",
+    "./src/components/**/*.{html,js,tsx,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
