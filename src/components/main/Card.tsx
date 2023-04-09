5acac3422c41aa9f80cd171199c6fef720b5fec2
hjw4595 <37586230+hjw4595@users.noreply.github.com>
2023-04-09 17:53:37 +0900

    feature : main page layout (#9)

---
+++ src/components/main/Card.tsx
@@ -0,0 +1,27 @@
+const IconWrapper = () => (
+  <div className="h-[24px] w-[24px] border-[1px] border-black m-[1px]"></div>
+);
+
+function Card() {
+  return (
+    <div className="relative w-1/2">
+      <div className="absolute top-[10px] right-0 flex flex-wrap w-[60px]">
+        {IconWrapper()}
+        {IconWrapper()}
+        {IconWrapper()}
+        {IconWrapper()}
+      </div>
+      <div className="h-[187px]"></div>
+      <div className="p-[16px] h-[118px] bg-[#FAFAFA]">
+        <div className="flex justify-between">
+          <div className="font-medium text-lg mb-[10px]">Title</div>
+          {IconWrapper()}
+        </div>
+        <div className="text-sm  mb-[10px]">adress</div>
+        <div className="font-medium text-sm">category</div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
