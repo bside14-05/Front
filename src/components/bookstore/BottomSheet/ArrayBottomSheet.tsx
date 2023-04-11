25e4a4cf90ff8fed29d4cc7c5245fb8284dabcc1
Chansu Kim <ckstn0777@naver.com>
2023-04-11 22:27:32 +0900

    fix: build issue

--- src/components/bookstore/BottomSheet/ArrayBottomSheet.tsx
+++ src/components/bookstore/BottomSheet/ArrayBottomSheet.tsx
@@ -2,34 +2,34 @@ import Button from "@/components/common/button";
 import Icon from "@/components/common/icon";
 import BottomSheet from "@/components/template/BottomSheet";
 
-// export default function FilterBottomSheet() {
-//   return (
-//     <BottomSheet>
-//       <h2 className="text-2xl font-semibold mb-7">Fillter</h2>
+export default function ArrayBottomSheet({ onClose }: { onClose: () => void }) {
+  return (
+    <BottomSheet onClose={onClose}>
+      <h2 className="text-2xl font-semibold mb-7">Fillter</h2>
 
-//       <ul className="flex flex-col gap-4 text-lg mb-12">
-//         <li className="flex justify-between py-px">
-//           <span>독립출판물</span>
-//           <Icon name="bookmark" />
-//         </li>
-//         <li className="flex justify-between py-px">
-//           <span>전문서적</span>
-//           <Icon name="bookmark" />
-//         </li>
-//         <li className="flex justify-between py-px">
-//           <span>종합서적</span>
-//           <Icon name="bookmark" />
-//         </li>
-//         <li className="flex justify-between py-px">
-//           <span>해외서적</span>
-//           <Icon name="bookmark" />
-//         </li>
-//       </ul>
+      <ul className="flex flex-col gap-4 text-lg mb-12">
+        <li className="flex justify-between py-px">
+          <span>독립출판물</span>
+          <Icon name="bookmark" />
+        </li>
+        <li className="flex justify-between py-px">
+          <span>전문서적</span>
+          <Icon name="bookmark" />
+        </li>
+        <li className="flex justify-between py-px">
+          <span>종합서적</span>
+          <Icon name="bookmark" />
+        </li>
+        <li className="flex justify-between py-px">
+          <span>해외서적</span>
+          <Icon name="bookmark" />
+        </li>
+      </ul>
 
-//       <div className="flex gap-2">
-//         <Button type="outline">초기화</Button>
-//         <Button type="normal">적용</Button>
-//       </div>
-//     </BottomSheet>
-//   );
-// }
+      <div className="flex gap-2">
+        <Button type="outline">초기화</Button>
+        <Button type="normal">적용</Button>
+      </div>
+    </BottomSheet>
+  );
+}
