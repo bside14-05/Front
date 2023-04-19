7a576509a6d96f4bfef30e687ec64c5743f03c68
hjw4595 <hongjung73@gmail.com>
2023-04-19 17:41:39 +0900

    my 1차레이아웃 작성

---
+++ src/components/linkList/UserInfo.tsx
@@ -0,0 +1,10 @@
+function UserInfo() {
+  return (
+    <section className="flex mb-4  justify-between border-b-2 border-black py-4">
+      <div className="py-6 text-6xl">MY</div>
+      <div className="py-6 text-lg self-end">asdasdasd@asdasdad.com</div>
+    </section>
+  );
+}
+
+export default UserInfo;
