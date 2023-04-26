e6316c65a61c4dc4de3b817a95031d1952376de1
hjw4595 <hongjung73@gmail.com>
2023-04-26 17:59:11 +0900

    add : header sticky

--- src/components/template/Header/Header.tsx
+++ src/components/template/Header/Header.tsx
@@ -2,7 +2,7 @@ import Icon from "@/components/common/icon";
 
 export default function Header({ onLNBOpen }: { onLNBOpen: () => void }) {
   return (
-    <header className="px-4 py-5 flex flex-row-reverse">
+    <header className="sticky top-0 px-4 py-5 flex flex-row-reverse bg-white z-50">
       <button onClick={onLNBOpen}>
         <Icon name="menu" />
       </button>
